a4e79f8cd6be04ac708c6207bfa0f1eac15d8560
Juan Perez <juan.perez@bigcommerce.com>
2023-01-21 02:55:08 -0600

    feat: Create Book model

---
+++ db/migrate/20230121083037_create_books.rb
@@ -0,0 +1,24 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.bigint "book_id", null: false
+      t.string "title", null: false
+      t.string "authors", array: true, default: []
+      t.string "publisher"
+      t.text "description"
+      t.string "language"
+      t.string "country"
+      t.bigint "isbn10"
+      t.bigint "isbn13"
+      t.string "ddc"
+      t.string "lcc"
+      t.string "categories", array: true, default:[]
+      t.integer "page_count"
+      t.date "published_date"
+      t.string "suggested_classifications", array: true, default: []
+      t.string "error_message"
+
+      t.timestamps
+    end
+  end
+end
