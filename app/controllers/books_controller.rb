8650dd5e1b849738ad8bf90c9790b94caad5b3e2
Juan Perez <juan.perez@bigcommerce.com>
2023-01-22 00:20:49 -0600

    feat: Add bulk creation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,27 +5,44 @@ class BooksController < ApplicationController
   include Alexandrite
 
   before_action :get_book_data, only: :create
+  before_action :get_bulk_data, only: :bulk
 
-  def create    binding.pry
-    book = @book.except(:data_source)
-    book[:book_id] = params['query'].to_i
 
-    unless book[:error_message]
-      return Book.create(book)
-    end
+  def create
+    return Book.create(@book.except(:key)) unless @book[:error_message]
 
-    error_message = {:message => @book[:error_message],
-                     :origin => @book[:data_source],
-                     :details => "Key: #{params['key']}. Query: #{params['query']}"}
+    error_message = create_error_message(@book)
 
     ErrorMessage.create(error_message)
   end
 
-  def bulk_new
-    binding.pry
+  def bulk
+    key = params['key']
+    data = params['data'].split(',')
+    count = { created: 0,
+              errors: 0
+    }
+
+    @books.each_with_index do |book, i|
+      book[:book_id] = data[i]
+      book[:key] = key
+
+      if book[:error_message]
+        count[:errors] += 1
+        error_message = create_error_message(book)
+
+        ErrorMessage.create(error_message)
+      else
+        count[:created] += 1
+        Book.create(book.except(:key))
+      end
+    end
+
+    render plain: "Added #{count[:created]} new books. #{count[:errors]} books failed. Check logs for more details",
+           status: :ok
   end
 
-  def index
+  def ddc
 
   end
 
@@ -41,6 +58,19 @@ class BooksController < ApplicationController
     key = params['key']
     query = params['query']
     @book = create_book(key, query)
+    @book[:book_id], @book[:key] = query.to_i, key
+  end
+
+  def create_error_message(info)
+    {:message => info[:error_message],
+    :origin => info[:data_source],
+    :details => "Key: #{info[:key]}. Query: #{info[:book_id]}"}
+  end
+
+  def get_bulk_data
+    key = params['key']
+    data = params['data'].split(',')
+    @books = bulk_create(key, data)
   end
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "books#create", defaults: { format: :json }
+
+  post 'books/bulk', defaults: { format: :json }
 end
