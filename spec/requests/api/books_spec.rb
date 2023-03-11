4fe757e146c7e58a08085f4502d15efeab55a1ab
Juan Perez <juan.perez@bigcommerce.com>
2023-03-11 17:09:58 -0600

    docs: swagger Add new cases

--- spec/requests/api/books_spec.rb
+++ spec/requests/api/books_spec.rb
@@ -3,16 +3,33 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/books', type: :request do
+
   path '/books/create' do
-    post 'Add a book' do
+    post 'Add a new book' do
       tags 'Books'
       consumes 'application/json'
-      parameter name: :key, in: :query, type: :string
-      parameter name: :query, in: :query, type: :string
+      parameter name: :key, in: :query, type: :string, description: "Add a field to query, such as: ISBN, Author or Title"
+      parameter name: :query, in: :query, type: :string, description: "Add a query"
 
-      response '200', 'book created' do
+      response '200', 'A book was created' do
+
         let(:book) { { key: 'isbn', query: '9789682709302' } }
         run_test!
+      end
+    end
+
+    path '/books/bulk' do
+      post('bulk create books') do
+        tags 'Books'
+        consumes 'multipart/form-data'
+        parameter name: :key, in: :query, type: :string, description: "A field to query, such as ISBN or ISSN"
+        parameter name: :data, in: :formData, type: :file, description: "A file with txt extension, with a value in each new line"
+
+        response '200', 'books created' do
+          let(:file_uploaded) { fixture_file_upload('files/isbns.txt', 'text/txt') }
+          let(:books) { { key:'isbn', data: :file_uploaded}}
+          run_test!
+        end
       end
     end
 
@@ -26,6 +43,20 @@ RSpec.describe 'api/books', type: :request do
               }
             }
           end
+          run_test!
+        end
+      end
+    end
+
+    path '/books/delete' do
+      delete('Delete a book') do
+        tags 'Books'
+        consumes 'application/json'
+        parameter name: :id, in: :query, type: :string, description: "ID of a book in the database"
+
+        response(200, 'successful') do
+          let(:id) { 1 }
+          let(:delete) { { id: id } }
           run_test!
         end
       end
